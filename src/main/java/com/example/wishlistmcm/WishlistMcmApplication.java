b5745ead20eaaa5e92cdb2c029e90ab5e5006abc
Mathias-Thomsen <math831t6@stud.kea.dk>
2023-03-27 09:38:05 +0200

    First commit - created packages

---
+++ src/main/java/com/example/wishlistmcm/WishlistMcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistmcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistMcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistMcmApplication.class, args);
+    }
+
+}
