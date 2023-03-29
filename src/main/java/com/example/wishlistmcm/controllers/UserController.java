fcaadb0037105d02e9b3917325aa9b74a9fc3576
ChristianMN <christian.munknissen@gmail.com>
2023-03-29 09:42:30 +0200

    sign up

--- src/main/java/com/example/wishlistmcm/controllers/UserController.java
+++ src/main/java/com/example/wishlistmcm/controllers/UserController.java
@@ -4,37 +4,47 @@ import com.example.wishlistmcm.repositories.DbManager;
 import com.example.wishlistmcm.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping({"wishlistmcm"})
+@RequestMapping({""})
 public class UserController {
 
     UserService userService;
 
     @GetMapping(value = {"/"})
-    public String index(){
+    public String index() {
         return "index";
     }
 
-   @GetMapping(value = {"/login"})
-        public String login(){
-            return "login";
-       }
+    @GetMapping(value = {"/login"})
+    public String login() {
+        return "login";
+    }
 
-       @GetMapping(value = {"/signup"})
-       public String create(){
+    @GetMapping(value = {"/signup"})
+    public String create() {
         return "signup";
-       }
+    }
 
-       @GetMapping(value = {"/wishlists"})
-    public String wishlists(){
+    @PostMapping("/signup")
+    public String processSignup(@RequestParam("email") String email,
+                                @RequestParam("fullname") String fullname,
+                                @RequestParam("password") String password) {
+        return "redirect:/";
+    }
+
+
+    @GetMapping(value = {"/wishlists"})
+    public String wishlists() {
         return "wishlists";
-       }
+    }
 
-       @GetMapping(value = {"/wishes"})
-    public String wishes(){
+    @GetMapping(value = {"/wishes"})
+    public String wishes() {
         return "wishes";
-       }
+    }
 
 }
