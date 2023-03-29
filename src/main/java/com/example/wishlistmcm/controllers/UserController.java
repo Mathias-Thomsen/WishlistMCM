92fe47c6c98ebb8338f2036477edee8b0046ee5c
ChristianMN <christian.munknissen@gmail.com>
2023-03-29 09:07:50 +0200

    get connection to localhost:8088 index.html

--- src/main/java/com/example/wishlistmcm/controllers/UserController.java
+++ src/main/java/com/example/wishlistmcm/controllers/UserController.java
@@ -1,5 +1,20 @@
 package com.example.wishlistmcm.controllers;
 
+import com.example.wishlistmcm.repositories.DbManager;
+import com.example.wishlistmcm.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"wishlistmcm"})
 public class UserController {
-    private String hello;
+
+    UserService userService;
+
+    @GetMapping(value = {"/"})
+    public String index(){
+        return "index";
+    }
+
 }
