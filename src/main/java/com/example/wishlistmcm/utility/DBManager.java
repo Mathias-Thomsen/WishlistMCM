6c21c8b5169184962d18622cc09393c560f536bf
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2023-04-17 14:35:47 +0000

    fixed

--- src/main/java/com/example/wishlistmcm/utility/DBManager.java
+++ src/main/java/com/example/wishlistmcm/utility/DBManager.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBManager {
-    private static Connection con = null;
+    
 
     public static Connection getConnection(){
 
