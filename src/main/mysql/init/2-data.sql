bfaa89420758685bc1518604b2b27f375282eb5f
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2023-04-17 11:37:37 +0000

    fixed sql

--- src/main/mysql/init/2-data.sql
+++ src/main/mysql/init/2-data.sql
@@ -1,4 +1,4 @@
-use Wishlist;
+use WishList;
 
 INSERT INTO USER (FULLNAME, EMAIL, USER_PASSWORD)
 VALUES
@@ -18,4 +18,4 @@ VALUES
 (1, 'iPhone 13', 'https://www.apple.com/shop/buy-iphone/iphone-13', 'The latest iPhone from Apple', 1099.00, 1),
 (2, 'Nintendo Switch', 'https://www.nintendo.com/switch/', 'A popular gaming console from Nintendo', 299.00, 1),
 (3, 'Amazon Gift Card', 'https://www.amazon.com/gift-cards', 'A gift card for online shopping', 50.00, 3 );
-commit;
+
