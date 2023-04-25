9d5a3a2cbbbb7f98309940778bb4ce80a47bc307
Mathias-Thomsen <math831t6@stud.kea.dk>
2023-04-25 21:37:17 +0200

    Change msql in properties

--- src/mysql/init/1-schema.sql
+++ src/mysql/init/1-schema.sql
@@ -1,30 +1,30 @@
-DROP DATABASE IF EXISTS wishlist;
 CREATE DATABASE wishlist;
-USE wishlist; 
+USE wishlist;
+DROP DATABASE IF EXISTS wishlist;
 
 CREATE TABLE user (
-    user_id INT AUTO_INCREMENT,
-    fullname VARCHAR(255),
-    email VARCHAR(255),
-    user_password VARCHAR(255),
-    PRIMARY KEY(user_id)
+  user_id INT AUTO_INCREMENT,
+  fullname VARCHAR(255),
+  email VARCHAR(255),
+  user_password VARCHAR(255),
+  PRIMARY KEY(user_id)
 );
 
 CREATE TABLE wishlist (
-    wishlist_id INT AUTO_INCREMENT,
-    wishlist_name VARCHAR(255),
-    user_id INT,
-    PRIMARY KEY(wishlist_id),
-    FOREIGN KEY(user_id) REFERENCES user(user_id)
+  wishlist_id INT AUTO_INCREMENT,
+  wishlist_name VARCHAR(255),
+  user_id INT,
+  PRIMARY KEY(wishlist_id),
+  FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
 
 CREATE TABLE wish (
-    wish_id INT AUTO_INCREMENT,
-    wish_name VARCHAR(255),
-    link_to_wish VARCHAR(2000),
-    wish_description VARCHAR(6000),
-    wish_price DECIMAL,
-    wishlist_id INT,
-    PRIMARY KEY(wish_id),
-    FOREIGN KEY(wishlist_id) REFERENCES wishlist(wishlist_id)
+  wish_id INT AUTO_INCREMENT,
+  wish_name VARCHAR(255),
+  link_to_wish VARCHAR(2000),
+  wish_description VARCHAR(6000),
+  wish_price DECIMAL,
+  wishlist_id INT,
+  PRIMARY KEY(wish_id),
+  FOREIGN KEY(wishlist_id) REFERENCES wishlist(wishlist_id)
 );
